8b145834b0d74316a22a623084ba501ea3e8aa88
Kathryn Butler <kbutle25@my.chemeketa.edu>
2024-12-02 13:11:56 -0800

    updated favicon and landing page

--- reactConversion/client/src/routes/landing.jsx
+++ reactConversion/client/src/routes/landing.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import homeImg from "../assets/welcomeCat.svg"
+import homeImg from "../assets/cat_coffee_cup.png"
 import homeImg2 from "../assets/parkCat.svg"
 
 const Landing = () => {
@@ -10,12 +10,12 @@ const Landing = () => {
         <div className="hero-content flex-col lg:flex-row max-w-[75%]">
           <img
             src={homeImg}
-            className="max-w-sm rounded-lg shadow-2xl" />
+            className="max-w-sm" />
           <div>
             <h1 className="text-5xl font-bold">Welcome to the Paws & Pastries Cat Cafe!</h1>
             <p className="py-6">
-              We partner with the local Humane Society to provide a living space for
-              15-25 cats at a time, and help connect felines to forever families.
+              At Paws & Pastries, we believe every cat deserves a loving home - and we're here to 
+              make that happen while offering you a cozy spot to sip, snack, and relax!
             </p>
           </div>
         </div>
@@ -25,11 +25,13 @@ const Landing = () => {
           <img
             src={homeImg2}
             className="max-w-sm rounded-lg shadow-2xl" />
-          <div>
-            <p className="py-6">
-              Patrons can study and purchase drinks from
-              the cafe, or relax and play with the cats in the cat lounge.
-            </p>
+          <div className="py-6">
+            <p>We partner with the local Humane Society to provide a living space for up to 25 cats at a time, and help connect felines to forever families.</p>
+            <p>Through this partnership, we've facilitated over 150 adoptions annually!</p>
+            <p>Feel free to unwind in our dedicated cat lounge where you can play, cuddle, and connect with the cats. 
+              You can also enjoy delicious drinks such as the Cat-ppuccino, the Meowcha Latte, or (our holiday favorite) a warm cup of Pep-paw-mint Tea.</p>
+            <p>Every purchase, from a latte to a kitty sweater, helps us continue our mission to connect cats with loving families. 
+              Thank you for being a part of our journey to make a difference!</p>
           </div>
         </div>
       </div>
